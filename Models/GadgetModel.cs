b75ac3af7c61081c2ea36781b1c199bb1d00bc1d
JaisonVT <jaisonvthomas@hotmail.com>
2021-09-11 17:14:14 +0530

    Add project files.

---
+++ Models/GadgetModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BondGadgetCollection.Models
+{
+    public class GadgetModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string ApperasIn { get; set; }
+        public string WithThisActor { get; set; }
+
+    }
+}
